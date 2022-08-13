474fc3cf1263bc926289c30f923f94945834c9c3
WooJinDeve <jwj060111@gmail.com>
2022-08-13 22:31:03 +0900

    업데이트

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+
+const config = require("./config/dev.js");
+const mongoose = require('mongoose')
+mongoose.connect(config.mongoURI)
+    .then(() => console.log('MongoDB Connected..'))
+    .catch(err => console.log(err))
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
